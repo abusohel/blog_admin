66ef674353b252b4d906bd1e4d0fde31950e0759
abu sohel <abusoheljnu@gmail.com>
2018-03-02 14:42:25 +0600

    after to 16 episode

--- resources/views/user/post.blade.php
+++ resources/views/user/post.blade.php
@@ -5,6 +5,16 @@
 @section('sub-heading',$post->subtitle)
 
 @section('main-content')
+
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.12&appId=214007626009382&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <!-- Post Content -->
 <article>
     <div class="container">
@@ -26,6 +36,7 @@
                 </small>
                 @endforeach
             </div>
+           <div class="fb-comments" data-href="https://127.0.0.1:8000" data-numposts="5"></div>
         </div>
     </div>
 </article>
